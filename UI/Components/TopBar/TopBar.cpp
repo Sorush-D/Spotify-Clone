1f98382590fa009f2edb535acc9696759883a46e
sorush <sorush195@gmail.com>
2026-07-13 23:55:17 +0330

    Complete top bar

---
+++ UI/Components/TopBar/TopBar.cpp
@@ -0,0 +1,95 @@
+#include "TopBar.h"
+#include <QHBoxLayout>
+#include "../../../Services/AuthenticationService/AuthenticationService.h"
+
+TopBar::TopBar(QWidget *parent) : QWidget(parent) {
+    setupUI();
+    setupConnections();
+    refreshUserState();
+}
+
+
+void TopBar::setupUI() {
+    logoButton = new QPushButton("Logo", this);
+    logoButton->setIcon(QIcon(":/Icons/logo.png"));
+
+    homeButton = new QPushButton("Home", this);
+    homeButton->setIcon(QIcon(":/Icons/home.png"));
+
+    searchEdit = new QLineEdit(this);
+    searchEdit->setPlaceholderText("Search...");
+
+    loginButton = new QPushButton("Login", this);
+    registerButton = new QPushButton("Sign Up", this);
+
+    profileButton = new QToolButton(this);
+    profileButton->setText("Profile");
+    profileButton->setIcon(QIcon(":/Icons/user.png"));
+
+    profileMenu = new QMenu(this);
+    profileAction = profileMenu->addAction("Profile");
+    editProfileAction = profileMenu->addAction("Edit Profile");
+    profileMenu->addSeparator();
+    logoutAction = profileMenu->addAction("Logout");
+
+    profileButton->setMenu(profileMenu);
+    profileButton->setPopupMode(QToolButton::MenuButtonPopup);
+    profileButton->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
+
+    auto *layout = new QHBoxLayout(this);
+
+    layout->addWidget(logoButton);
+    layout->addWidget(homeButton);
+
+    layout->addWidget(searchEdit, 1);
+
+    layout->addWidget(loginButton);
+    layout->addWidget(registerButton);
+    layout->addWidget(profileButton);
+}
+
+
+void TopBar::setupConnections() {
+    connect(homeButton, &QPushButton::clicked, this, &TopBar::homeClicked);
+    connect(searchEdit, &QLineEdit::returnPressed, this, [this]() {
+        emit searchRequested(searchEdit->text());
+    });
+    connect(loginButton, &QPushButton::clicked, this, &TopBar::loginClicked);
+    connect(registerButton, &QPushButton::clicked, this, &TopBar::registerClicked);
+
+    connect(profileButton, &QToolButton::clicked, this, &TopBar::profileClicked);
+    connect(profileAction, &QAction::triggered, this, &TopBar::profileClicked);
+    connect(editProfileAction, &QAction::triggered, this, &TopBar::editProfileClicked);
+    connect(logoutAction, &QAction::triggered, this, &TopBar::logoutClicked);
+}
+
+
+void TopBar::refreshUserState() {
+    auto &auth = AuthenticationService::instance();
+    if (auth.isLoggedIn()) {
+        loginButton->hide();
+        registerButton->hide();
+
+        profileButton->show();
+
+        if (auth.currentArtist()) {
+            profileButton->setText(auth.currentArtist()->getUserName());
+            profileButton->setIcon(profileIcon(auth.currentArtist()->getProfilePicture()));
+        } else if (auth.currentListener()) {
+            profileButton->setText(auth.currentListener()->getUserName());
+            profileButton->setIcon(profileIcon(auth.currentListener()->getProfilePicture()));
+        }
+    } else {
+        loginButton->show();
+        registerButton->show();
+
+        profileButton->hide();
+    }
+}
+
+
+QIcon TopBar::profileIcon(const QByteArray &image) const {
+    QPixmap pixmap;
+    if (image.isEmpty() || !pixmap.loadFromData(image)) return QIcon(":/Icons/user.png");
+    return QIcon(pixmap);
+}
---
+++ UI/Components/TopBar/TopBar.h
@@ -0,0 +1,55 @@
+#ifndef SPOTIFY_CLONE_TOPBAR_H
+#define SPOTIFY_CLONE_TOPBAR_H
+#include <QPushButton>
+#include <QLineEdit>
+#include <QToolButton>
+#include <QMenu>
+#include <qwidget.h>
+
+
+class TopBar : public QWidget {
+    Q_OBJECT
+    QPushButton *logoButton;
+    QPushButton *homeButton;
+
+    QLineEdit *searchEdit;
+
+    QPushButton *loginButton;
+    QPushButton *registerButton;
+
+    QToolButton *profileButton;
+
+    QMenu *profileMenu;
+    QAction *profileAction;
+    QAction *editProfileAction;
+    QAction *logoutAction;
+
+    void setupUI();
+
+    void setupConnections();
+
+    void refreshUserState();
+
+    QIcon profileIcon(const QByteArray &) const;
+
+public:
+    explicit TopBar(QWidget *parent = nullptr);
+
+signals:
+    void homeClicked();
+
+    void searchRequested(const QString &);
+
+    void loginClicked();
+
+    void registerClicked();
+
+    void profileClicked();
+
+    void editProfileClicked();
+
+    void logoutClicked();
+};
+
+
+#endif
