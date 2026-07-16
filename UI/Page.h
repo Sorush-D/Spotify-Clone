78eb0d5b4eefb5608805605b7373d6fec64c8ce8
sorush <sorush195@gmail.com>
2026-07-17 02:24:27 +0330

    Create enum class for pages

---
+++ UI/Page.h
@@ -0,0 +1,32 @@
+#ifndef SPOTIFY_CLONE_PAGE_H
+#define SPOTIFY_CLONE_PAGE_H
+
+
+enum class Page {
+    Login,
+    Register,
+
+    Home,
+    Search,
+
+    Playlist,
+    Album,
+    LikedSongs,
+
+    Profile,
+    ProfileEdit,
+
+    ArtistProfile,
+
+    CreateAlbum,
+    EditAlbum,
+
+    CreatePlaylist,
+    EditPlaylist,
+
+    CreateSong,
+    EditSong
+};
+
+
+#endif
