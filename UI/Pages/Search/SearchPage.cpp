59fb6751066f766dd35cae28029cab21cec490ef
sorush <sorush195@gmail.com>
2026-07-16 14:33:56 +0330

    Compelete search page

---
+++ UI/Pages/Search/SearchPage.cpp
@@ -0,0 +1,82 @@
+#include "SearchPage.h"
+#include <QMenu>
+
+SearchPage::SearchPage(QWidget *parent) : QWidget(parent) {
+    setupUI();
+    setupMenus();
+}
+
+
+void SearchPage::setupUI() {
+    sortButton = new QToolButton(this);
+    sortButton->setText("Sort");
+
+    filterButton = new QToolButton(this);
+    filterButton->setText("Filter");
+
+    songsList = new CardList<SongCard, Song>("Songs", {}, this);
+    albumsList = new CardList<AlbumCard, Album>("Albums", {}, this);
+    artistsList = new CardList<ArtistCard, Artist>("Artists", {}, this);
+
+    auto *toolsLayout = new QHBoxLayout;
+    toolsLayout->addWidget(filterButton);
+    toolsLayout->addWidget(sortButton);
+    toolsLayout->addStretch();
+
+    auto *layout = new QVBoxLayout(this);
+
+    layout->addLayout(toolsLayout);
+
+    layout->addWidget(songsList);
+    layout->addWidget(albumsList);
+    layout->addWidget(artistsList);
+}
+
+
+void SearchPage::setupMenus() {
+    auto *sortMenu = new QMenu(this);
+
+    auto *titleAsc = sortMenu->addAction("Title ↑");
+    auto *titleDesc = sortMenu->addAction("Title ↓");
+    auto *yearAsc = sortMenu->addAction("Release Year ↑");
+    auto *yearDesc = sortMenu->addAction("Release Year ↓");
+
+    connect(titleAsc, &QAction::triggered, this, [this] {
+        emit sortSongsRequested(SongSort::Title, SortOrder::Ascending);
+    });
+    connect(titleDesc, &QAction::triggered, this, [this] {
+        emit sortSongsRequested(SongSort::Title, SortOrder::Descending);
+    });
+    connect(yearAsc, &QAction::triggered, this, [this] {
+        emit sortSongsRequested(SongSort::ReleaseYear, SortOrder::Ascending);
+    });
+    connect(yearDesc, &QAction::triggered, this, [this] {
+        emit sortSongsRequested(SongSort::ReleaseYear, SortOrder::Descending);
+    });
+
+    sortButton->setMenu(sortMenu);
+    sortButton->setPopupMode(QToolButton::InstantPopup);
+
+
+    auto *filterMenu = new QMenu(this);
+
+    filterMenu->addAction("Coming soon");
+
+    filterButton->setMenu(filterMenu);
+    filterButton->setPopupMode(QToolButton::InstantPopup);
+}
+
+
+void SearchPage::setAlbums(const QVector<Album> &albums) {
+    albumsList->setItems(albums);
+}
+
+
+void SearchPage::setArtists(const QVector<Artist> &artists) {
+    artistsList->setItems(artists);
+}
+
+
+void SearchPage::setSongs(const QVector<Song> &songs) {
+    songsList->setItems(songs);
+}
---
+++ UI/Pages/Search/SearchPage.h
@@ -0,0 +1,42 @@
+#ifndef SPOTIFY_CLONE_SEARCHPAGE_H
+#define SPOTIFY_CLONE_SEARCHPAGE_H
+#include "../../Components/CardList/CardList.h"
+#include "../../Components/Cards/AlbumCard/AlbumCard.h"
+#include "../../Components/Cards/ArtistCard/ArtistCard.h"
+#include "../../Components/Cards/SongCard/SongCard.h"
+#include "../../../Services/BrowseService/BrowseService.h"
+
+
+class SearchPage : public QWidget {
+    Q_OBJECT
+
+    QToolButton *sortButton;
+    QToolButton *filterButton;
+
+    CardList<SongCard, Song> *songsList;
+    CardList<AlbumCard, Album> *albumsList;
+    CardList<ArtistCard, Artist> *artistsList;
+
+    void setupUI();
+
+    void setupMenus();
+
+public:
+    explicit SearchPage(QWidget *parent = nullptr);
+
+    void setSongs(const QVector<Song> &);
+
+    void setAlbums(const QVector<Album> &);
+
+    void setArtists(const QVector<Artist> &);
+
+signals:
+    void sortSongsRequested(SongSort, SortOrder);
+
+    void filterGenreRequested(const QString &);
+
+    void filterReleaseYearRequested(int);
+};
+
+
+#endif
