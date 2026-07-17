2dec95678fd8e9e23976f6a788be1ecdc1e8d1bd
sorush <sorush195@gmail.com>
2026-07-17 18:33:57 +0330

    Change album page to shows albums instead of showing songs of a album

--- UI/Pages/Album/AlbumPage.cpp
+++ UI/Pages/Album/AlbumPage.cpp
@@ -1,6 +1,7 @@
 #include "AlbumPage.h"
 #include <QVBoxLayout>
 #include "../../../Repositories/ArtistRepository/ArtistRepository.h"
+#include "../../Components/Cards/AlbumCard/AlbumCard.h"
 
 AlbumPage::AlbumPage(QWidget *parent) : QWidget(parent) {
     setupUI();
@@ -8,42 +9,13 @@ AlbumPage::AlbumPage(QWidget *parent) : QWidget(parent) {
 
 
 void AlbumPage::setupUI() {
-    coverLabel = new QLabel(this);
-    coverLabel->setFixedSize(250, 250);
-    coverLabel->setScaledContents(true);
-    coverLabel->setPixmap(QPixmap(":/Icons/album.png"));
-
-    titleLabel = new QLabel(this);
-    artistLabel = new QLabel(this);
-
-    songsGrid = new CardGrid<SongCard, Song>("Songs", this);
+    albumsGrid = new CardGrid<AlbumCard, Album>("Albums", this);
 
     auto *layout = new QVBoxLayout(this);
-
-    layout->addWidget(coverLabel);
-    layout->addWidget(titleLabel);
-    layout->addWidget(artistLabel);
-
-    layout->addWidget(songsGrid);
+    layout->addWidget(albumsGrid);
 }
 
 
-void AlbumPage::setAlbum(const Album &album) {
-    coverLabel->setPixmap(getCover(album.getCoverPicture()));
-    titleLabel->setText(album.getTitle());
-
-    const auto artist = ArtistRepository::instance().search(album.getArtistID());
-    artistLabel->setText(artist.has_value() ? artist->getFullName() : "Unknown Artist");
-}
-
-
-void AlbumPage::setSongs(const QVector<Song> &songs) {
-    songsGrid->setItems(songs);
-}
-
-
-QPixmap AlbumPage::getCover(const QByteArray &image) const {
-    QPixmap pixmap;
-    if (image.isEmpty() || !pixmap.loadFromData(image)) return QPixmap(":/Icons/album.png");
-    return pixmap;
+void AlbumPage::setAlbums(const QVector<Album> &albums) {
+    albumsGrid->setItems(albums);
 }
--- UI/Pages/Album/AlbumPage.h
+++ UI/Pages/Album/AlbumPage.h
@@ -1,7 +1,7 @@
 #ifndef SPOTIFY_CLONE_ALBUMPAGE_H
 #define SPOTIFY_CLONE_ALBUMPAGE_H
 #include "../../Components/CardGrid/CardGrid.h"
-#include "../../Components/Cards/SongCard/SongCard.h"
+#include "../../Components/Cards/AlbumCard/AlbumCard.h"
 #include "../../../Entities/Album/Album.h"
 #include <QWidget>
 
@@ -9,22 +9,14 @@
 class AlbumPage : public QWidget {
     Q_OBJECT
 
-    QLabel *coverLabel;
-    QLabel *titleLabel;
-    QLabel *artistLabel;
-
-    CardGrid<SongCard, Song> *songsGrid;
+    CardGrid<AlbumCard, Album> *albumsGrid;
 
     void setupUI();
-
-    QPixmap getCover(const QByteArray &) const;
 
 public:
     explicit AlbumPage(QWidget *parent = nullptr);
 
-    void setAlbum(const Album &);
-
-    void setSongs(const QVector<Song> &);
+    void setAlbums(const QVector<Album> &);
 };
 
 
