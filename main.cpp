8f01fdaa79129dad1c98be68d91463fc8614ea04
sorush <sorush195@gmail.com>
2026-07-18 12:35:30 +0330

    Complete project

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <QApplication>
-#include <QPushButton>
+#include "Database/DatabaseManager.h"
+#include "UI/MainWindow/MainWindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
+
+    if (!DatabaseManager::instance().open()) return -1;
+    if (!DatabaseManager::instance().createTables()) return -1;
+
+    MainWindow window;
+    window.show();
+
     return QApplication::exec();
 }
